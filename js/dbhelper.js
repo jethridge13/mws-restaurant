50b7958b7042764f697087b2dc95a5afc395129b
Josh Ethridge <jethridge@kmbs.konicaminolta.us>
2018-08-06 12:58:46 -0400

    feat: Improve lazy loading
    Once an image has been lazily loaded, it will now be removed
    from the intersection observer. Additionally, lazy loading will
    now only happen when the main thread is otherwise idle.

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -251,23 +251,25 @@ class DBHelper {
   }
 
   static handleIntersection(entries) {
-  // TODO Remove element from observer after it is called once.
   // There is no reason for it to be called a second time.
   // All code is my own original work. The following links were
   // consulted when working on it.
   // https://developers.google.com/web/updates/2016/04/intersectionobserver
   // https://scotch.io/tutorials/lazy-loading-images-for-performance-using-intersection-observer
-  entries.forEach(entry => {
-      if (entry.isIntersecting && entry.target.nodeName === 'PICTURE') {
-        Array.from(entry.target.children).forEach(child => {
-          if (child['data-fullsrc'] && child.src) {
-            child.src = child['data-fullsrc'];
-          } else if (child['data-fullsrc'] && child.srcset) {
-            child.srcset = child['data-fullsrc'];
-          }
-        });
-      }
-    })
+    window.requestIdleCallback(() => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting && entry.target.nodeName === 'PICTURE') {
+          Array.from(entry.target.children).forEach(child => {
+            if (child['data-fullsrc'] && child.src) {
+              child.src = child['data-fullsrc'];
+            } else if (child['data-fullsrc'] && child.srcset) {
+              child.srcset = child['data-fullsrc'];
+            }
+          });
+          observer.unobserve(entry.target);
+        }
+      })
+    });
   }
 
   static registerObserver() {
